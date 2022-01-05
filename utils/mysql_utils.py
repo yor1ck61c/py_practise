ca22cad54b274e128e12f3d8c91b6b0e14cc1d0c
yorick_61c <1240371570@qq.com>
2022-01-05 12:39:40 +0800

    update utils format

--- utils/mysql_utils.py
+++ utils/mysql_utils.py
@@ -1,13 +1,17 @@
 import pymysql
+
+
 class ConMysql:
+
     config = {
         "host": "127.0.0.1",
         "port": "3306",
         "user": "root",
         "password": "0601",
         "charset": "utf8",
-        "cursorclass": pymysql.cursors.DictCursor
+        "cursor_class": pymysql.cursors.DictCursor
     }
-    def __init__(self, config) {
+
+    def __init__(self, config): {
 
     }
