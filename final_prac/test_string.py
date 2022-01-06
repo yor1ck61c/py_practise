8237876031421ea31b593b34071866a0b845cbda
yor1ck61c <1240371570@qq.com>
2022-01-06 11:23:25 +0800

    finish two part of the dispose of data

--- final_prac/test_string.py
+++ final_prac/test_string.py
@@ -12,13 +12,27 @@
 #     is_AMG = 1
 # 5万公里／2017-05／上海／2年钻石商家
 str2 = "5万公里／2017-05／上海／2年钻石商家"
-mileage = 5 * 10000
-release_year = 2022
-release_month = 7
-release_area = ""
-seller_time = 2
-seller_type = ""
+infos = str2.split('／')
+
+mileage = int(infos[0].replace("万公里", "")) * 10000
+
+date_info = infos[1].split('-')
+release_year = int(date_info[0])
+release_month = int(date_info[1])
+
+release_area = infos[2]
+
+seller_info = infos[3].replace("年", "").replace("商家", "")
+seller_time = seller_info[0]
+seller_type = seller_info[1:3]
+
+print(mileage)
+print(release_year)
+print(release_month)
+print(release_area)
+print(seller_time)
+print(seller_type)
 
 # 26.98万 √
-def set_car_price(car_price):
-    return int(float(car_price.replace("万", "")) * 10000)
+# def set_car_price(car_price):
+#     return int(float(car_price.replace("万", "")) * 10000)
